1b7ca5d227aa81072d422d35a57d0fe01b03a442
yousif609230 <yousif.91.ahmed@gmail.com>
2022-10-11 21:10:13 +0300

    some modification

--- VPC-Module-BFC/outputs.tf
+++ VPC-Module-BFC/outputs.tf
@@ -1,13 +1,14 @@
 output "vpc_id" {
   description = "VPC ID"
-  value = aws_vpc.vpc.id
+  value       = aws_vpc.vpc.id
 }
 
 output "vpc_arn" {
   description = "VPC ARN"
-  value = aws_vpc.vpc.arn
+  value       = aws_vpc.vpc.arn
 }
 
 output "vpc_cidr" {
+  description = "VPC CIDR block"
   value = aws_vpc.vpc.cidr_block
 }
--- VPC-Module-BFC/variable.tf
+++ VPC-Module-BFC/variable.tf
@@ -1,20 +1,37 @@
 variable "vpc_cidr" {
   description = "cidr Block for VPC"
-  type = string
-  }
+  type        = string
+  default = "10.10.0.0/16"
+}
 
-  variable "enable_dns_support" {
-    description = "enable or disable dns support"
-    type = boolean
-    default = true
-  }
+variable "enable_dns_support" {
+  description = "enable or disable dns support"
+  type        = bool
+  default     = true
+}
 
-  variable "enable_dns_hostnames" {
-    description = "value"
-    default = false
-  }
+variable "enable_dns_hostnames" {
+  type = bool
+  description = "value"
+  default     = false
+}
 
-  variable "tags" {
-    description = "provide a tag for the resources"
-    default = ""
+variable "tags" {
+  type = map(string)
+  description = "provide a tag for the resources"
+  default     = {
+    "name" = "test"
   }
+}
+
+variable "azs" {
+  description = "availability zone"
+  type = list(string)
+  default = [ "us-east-2a" ]
+}
+
+variable "subnet_block" {
+  type = list(string)
+  default = [ "10.10.0.0/22" , "10.10.4.0/22" ]
+
+}
